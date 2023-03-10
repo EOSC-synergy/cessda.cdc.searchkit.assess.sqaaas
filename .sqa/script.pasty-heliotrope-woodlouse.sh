be54569889a74e89e12a5d26238e662359ac8792
orviz <orviz@ifca.unican.es>
2023-03-10 14:08:36 +0100

    Add JePL file structure

--- .sqa/script.pasty-heliotrope-woodlouse.sh
+++
@@ -1,4 +0,0 @@
-(
-cd bitbucket.org/cessda/cessda.cdc.searchkit &&
-    npx stylelint **/*.vue
-)
---
+++ .sqa/script.stuffy-myrtle-dunker.sh
@@ -0,0 +1,4 @@
+(
+cd github.com/cessda/cessda.cdc.searchkit &&
+    licensee detect . --confidence 60 --json
+)
